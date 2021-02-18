1d3ced0445907a8d420e00aa054c38cb7e2ef097
Enrico Oliva <enrich.91@gmail.com>
2021-02-18 15:50:50 +0100

    esercizio fatto

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual'è il tuo nome?");
+var cognome = prompt("Qual'è il tuo cognome?");
+var colore = prompt("Qual'è il tuo colore preferito?");
+
+document.getElementById("pass").innerHTML = nome + cognome + colore + "19";
